a8cae75ec6b1ad4674b0d74e3d11406e47e3a6c0
MiguelJiMo <dxnte12@gmail.com>
2025-01-25 01:07:17 -0400

    carrito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,40 @@ import { RouterOutlet } from '@angular/router';
 import { Componente1Component } from "./componente1/componente1.component";
 import { CardComponent } from './card/card.component';
 import { TskManagerComponent } from './tsk-manager/tsk-manager.component';
+import { CommonModule } from '@angular/common';
+
+
+interface Product {
+  nombre: string;
+  precio: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Componente1Component, CardComponent, TskManagerComponent],
+  imports: [RouterOutlet, CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Proyecto1';
+
+
+  products : Product[] = [
+    { nombre: 'Producto 1', precio: 100 },
+    { nombre: 'Producto 2', precio: 200 },
+    { nombre: 'Producto 3', precio: 300 },
+    { nombre: 'Producto 4', precio: 400 },
+    { nombre: 'Producto 5', precio: 500 },
+    { nombre: 'Producto 6', precio: 600 },
+    { nombre: 'Producto 7', precio: 700 },
+    { nombre: 'Producto 8', precio: 800 },
+    { nombre: 'Producto 9', precio: 900 },
+    { nombre: 'Producto 10', precio: 1000 },
+  ]
+  cart : Product[] = [];
+  
+  onAddToCart(product: Product) {
+    this.cart.push(product);
+    console.log('Carrito:', this.cart);
+  }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
   mensaje: string = 'hi world!!!!!!!';
+// Recibe un objeto de producto desde el componente padre
+  @Input() product : any;
+ // Emite un evento al hacer clic en el botón "Agregar al carrito"
+  @Output() addToCart = new EventEmitter<any>();
+
+  // Método para emitir el evento al hacer clic en el botón
+  onAddToCart() {
+    this.addToCart.emit(this.product);
+  }
 }
